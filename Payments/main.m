fd377a6994f9528cdeb9244abfe925238892b786
Mark Wong <markwong.digital@gmail.com>
2022-05-24 14:32:13 -0700

    Initial Commit

---
+++ Payments/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Mark Wong on 2022-05-24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
